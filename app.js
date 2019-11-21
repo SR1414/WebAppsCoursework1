7fabfeee1b803e4c4e8c16b0fb0f3e55ed7a77ab
SR1414 <SR1414@live.mdx.ac.uk>
2019-11-21 04:23:49 +0000

    ADDEDED COURSE VUE INSTANCE WITH COURSES

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-var courses = [
-  { 'topic': 'math', 'location': 'hendon', 'price': 100 },
-  { 'topic': 'math', 'location': 'colindale', 'price': 80 },
-  { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
-  { 'topic': 'math', 'location': 'golders green', 'price': 120 },
-  { 'topic': 'english', 'location': 'hendon', 'price': 110 },
-  { 'topic': 'english', 'location': 'colindale', 'price': 90 },
-  { 'topic': 'english', 'location': 'brent cross', 'price': 90 },
-  { 'topic': 'english', 'location': 'golders green', 'price': 130 },
-  { 'topic': 'piano', 'location': 'hendon', 'price': 120 },
-  { 'topic': 'piano', 'location': 'golders green', 'price': 140 }];
 
-var courses = new vue({
-  data: 
-  { 'topic': 'math', 'location': 'hendon', 'price': 100 },
-  { 'topic': 'math', 'location': 'colindale', 'price': 80 },
-  { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
-  { 'topic': 'math', 'location': 'golders green', 'price': 120 },
-  { 'topic': 'english', 'location': 'hendon', 'price': 110 },
-  { 'topic': 'english', 'location': 'colindale', 'price': 90 },
-  { 'topic': 'english', 'location': 'brent cross', 'price': 90 },
-  { 'topic': 'english', 'location': 'golders green', 'price': 130 },
-  { 'topic': 'piano', 'location': 'hendon', 'price': 120 },
-  { 'topic': 'piano', 'location': 'golders green', 'price': 140 }
-)
+var displaycourses = new Vue ({
+  el:'#courses',
+  data: {
+    courses: [
+      { 'topic': 'math', 'location': 'hendon', 'price': 100 },
+      { 'topic': 'math', 'location': 'colindale', 'price': 80 },
+      { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
+      { 'topic': 'math', 'location': 'golders green', 'price': 120 },
+      { 'topic': 'english', 'location': 'hendon', 'price': 110 },
+      { 'topic': 'english', 'location': 'colindale', 'price': 90 },
+      { 'topic': 'english', 'location': 'brent cross', 'price': 90 },
+      { 'topic': 'english', 'location': 'golders green', 'price': 130 },
+      { 'topic': 'piano', 'location': 'hendon', 'price': 120 },
+      { 'topic': 'piano', 'location': 'golders green', 'price': 140 }]
+    
+  }
+})
 
 ////////////////////////////////////SIGNUP////////////////////////////////////////
 var reg = new Vue({
