9595cb975436b7fd69247784eb6241175a04617f
SR1414 <SR1414@live.mdx.ac.uk>
2019-11-22 00:54:50 +0000

    ADD FULL FILTERING OPTIONS

--- app.js
+++ app.js
@@ -1,138 +1,175 @@
-var testvue = new Vue({
+/*var testvue = new Vue({
   el: '#root',
   data: {
     search: '',
+    checkedLocation: [],
+    filterRating: '',
     courses: [
       {
         topic: 'math',
         location: 'hendon',
-        price: 100
+        price: 100,
+        rating: 5
       },
       {
         topic: 'math',
-        location: 'colindale', 'price': 80
+        location: 'colindale',
+        price: 80,
+        rating: 3
+
       },
       {
         topic: 'math',
-        location: 'brent cross', 'price': 90
+        location: 'brent cross',
+        price: 90,
+        rating: 4
       },
       {
         topic: 'math',
         location: 'golders green',
-        price: 120
+        price: 120,
+        rating: 5
       },
       {
         topic: 'english',
         location: 'hendon',
-        price: 110
+        price: 110,
+        rating: 5
       },
       {
         topic: 'english',
         location: 'colindale',
-        price: 90
+        price: 90,
+        rating: 4
       },
       {
         topic: 'english',
         location: 'brent cross',
-        price: 90
+        price: 90,
+        rating: 2
       },
       {
         topic: 'english',
         location: 'golders green',
-        price: 130
+        price: 130,
+        rating: 5
       },
       {
-        topic: 'piano',
+        topic: 'sports',
         location: 'hendon',
-        price: 120
+        price: 120,
+        rating: 5
       },
       {
-        topic: 'piano',
+        topic: 'sports',
         location: 'golders green',
-        price: 140
+        price: 140,
+        rating: 4
       }]
   },
   computed: {
-    filteredCourses (){
-      if(this.search){
-      return this.courses.filter((course)=>{
-        return course.topic.startsWith(this.search);
-      })
-      }else{
+    filteredCourses() {
+      if (this.search) {
+        this.courses.filter((course) => {
+          course.topic.startsWith(this.search);
+        })
+        return this.course;
+      }
+      else {
         return this.courses;
       }
     }
   }
-})
-/*var displaycourses = new Vue({
-  el: '#example-2',
-  data: {
-    parentMessage: 'Course: ',
-    search: '',
-    courses: [
-      {
-        topic: 'math',
-        location: 'hendon',
-        price: 100
-      },
-      {
-        topic: 'math',
-        location: 'colindale', 'price': 80
-      },
-      {
-        topic: 'math',
-        location: 'brent cross', 'price': 90
-      },
-      {
-        topic: 'math',
-        location: 'golders green',
-        price: 120
-      },
-      {
-        topic: 'english',
-        location: 'hendon',
-        price: 110
-      },
-      {
-        topic: 'english',
-        location: 'colindale',
-        price: 90
-      },
-      {
-        topic: 'english',
-        location: 'brent cross',
-        price: 90
-      },
-      {
-        topic: 'english',
-        location: 'golders green',
-        price: 130
-      },
-      {
-        topic: 'piano',
-        location: 'hendon',
-        price: 120
-      },
-      {
-        topic: 'piano',
-        location: 'golders green',
-        price: 140
-      }]
-  },
-  computed: {
-  filteredCourses (){
-    if(this.search){
-    return this.courses.filter((course)=>{
-      return course.topic.startsWith(this.search);
-    })
-    }else{
-      return this.courses;
-    }
-  }
-}
-  
 })*/
 
+var testvue = new Vue({
+  el: '#root',
+  data: {
+    search: '',
+    checkedLocation: [],
+    filterRating: '',
+    filterPrice: '',
+    courses: [
+      {topic: 'math', location: 'hendon', price: 100, rating: 5},
+      {topic: 'math', location: 'colindale', price: 80, rating: 3},
+      {topic: 'math', location: 'brent cross', price: 90, rating: 4},
+      {topic: 'math', location: 'golders green', price: 120, rating: 5},
+      {topic: 'english', location: 'hendon', price: 110, rating: 5},
+      {topic: 'english', location: 'colindale', price: 90, rating: 4},
+      {topic: 'english', location: 'brent cross', price: 90, rating: 2},
+      {topic: 'english', location: 'golders green', price: 130, rating: 5},
+      {topic: 'sports', location: 'hendon', price: 120, rating: 5},
+      {topic: 'sports', location: 'golders green', price: 140, rating: 4}]
+  },methods: {
+    reset: function() {
+      this.search = '';
+      this.checkedLocation = [];
+      this.filterRating = '';
+      this.filterPrice = '';
+    }
+  },
+  computed: {
+    filteredCourses: function() {
+      var topics = this.search;
+      var locations = this.checkedLocation;
+      var rating = this.filterRating;
+      var price = this.filterPrice;
+      return this.courses.filter(function (course) {
+        var topicMatch = false;
+        var locationMatch = false;
+        var ratingMatch = false;
+        var priceMatch = false;
+        if (topics.length > 0) {
+          if (course.topic.startsWith(topics)) {
+            topicMatch = true;
+          };
+           }
+        else {
+          topicMatch = true;
+        }
+        if (locations.length > 0) {
+          if (locations.includes(course.location)) {
+            locationMatch = true;
+          }
+        }
+        else {
+          locationMatch = true;
+        }
+        if (rating.length > 0) {
+          if (rating.includes(course.rating)) {
+            ratingMatch = true;
+          }
+        }
+        else {
+          ratingMatch = true;
+        }
+        
+        if (price.length > 0) {
+          if (price >= course.price) {
+            priceMatch = true;
+          }
+        }
+        else {
+          priceMatch = true;
+        }
+        return topicMatch && locationMatch && ratingMatch && priceMatch;
+      })
+    }
+  }
+})
+
+
+
+function getByKeyword(courses, search) {
+  var searchQ = search.trim()
+  if (!searchQ.length) return list
+  return list.filter(item => item.name.indexOf(searchQ) > -1)
+}
+
+function getByCategory(list, category) {
+  if (!category) return list
+  return list.filter(item => item.category === category)
+}
 
 
 
@@ -144,7 +181,8 @@ var reg = new Vue({
     signfirstname: '',
     signlastname: '',
     signusertype: '',
-    signpassword: ''
+    signpassword: '',
+    seen: true
 
   },
   methods: {
@@ -165,8 +203,17 @@ var reg = new Vue({
           Password: password
         });
         var myuserlistserialized = JSON.stringify(userlist);
+        /*var i = 0;
+        for (i = 0; i < userlistserialized.length - 1; i++) {
+          if (userlistserialized[i].Email = this.email) {
+            alert("Email is already Registered")
+            console.log("yesy");
+            return;
+          }
+        }*/
         localStorage.setItem("MyUser", myuserlistserialized);
         console.log("ADDED IF DOESNT EXIST");
+        this.seen = false;
         alert("Account Created");
         return;
       };
@@ -183,6 +230,8 @@ var reg = new Vue({
         var myuserlistserialized = JSON.stringify(myuserlist_deserialized);
         localStorage.setItem("MyUser", myuserlistserialized);
         console.log("ADDED IF DOES EXIST");
+        this.seen = false;
+        return;
 
       };
     }
@@ -195,7 +244,8 @@ var loginuser = new Vue({
   el: '#loginform',
   data: {
     logemail: '',
-    logpassword: ''
+    logpassword: '',
+    seen: true
 
   },
   methods: {
@@ -212,6 +262,7 @@ var loginuser = new Vue({
           var v
           localStorage.setItem("LoggedInUser", currentuserJSON);
           console.log(currentuser);
+          this.seen = false;
           alert(
             "log In Successful. Hello " + currentuser.Firstname + "!"
           )
@@ -241,8 +292,117 @@ var userinfo = new Vue({
         this.Message = "Logged in"
       }
 
+    },
+    loguserout: function logout() {
+      if (localStorage.getItem("LoggedInUser") !== null) {
+        localStorage.removeItem("LoggedInUser");
+        this.Email = "-";
+        this.Firstname = "-";
+        this.Lastname = "-";
+        this.UserType = "-";
+        this.Message = "Not Logged In";
+        loginuser.seen = true;
+
+
+      }
+
     }
   }
 });
 
 
+var courses = [
+  { 'topic': 'math', 'location': 'hendon', 'price': 100 },
+  { 'topic': 'math', 'location': 'colindale', 'price': 80 },
+  { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
+  { 'topic': 'math', 'location': 'golders green', 'price': 120 },
+  { 'topic': 'english', 'location': 'hendon', 'price': 110 },
+  { 'topic': 'english', 'location': 'colindale', 'price': 90 },
+  { 'topic': 'english', 'location': 'brent cross', 'price': 90 },
+  { 'topic': 'english', 'location': 'golders green', 'price': 130 },
+  { 'topic': 'piano', 'location': 'hendon', 'price': 120 },
+  { 'topic': 'piano', 'location': 'golders green', 'price': 140 }];
+
+var filterApp = new Vue({
+  el: '#filter',
+  data: {
+    courses: courses,
+    selectedTopic: [],
+    selectedLocation: [],
+  },
+  methods: {
+    reset: function () {
+      this.selectedTopic = [];
+      this.selectedLocation = [];
+    }
+  }, computed: {
+    filteredCourses: function () {
+      var topics = this.selectedTopic, locations = this.selectedLocation;
+      return this.courses.filter(function (course) {
+        var topicMatch = false, locationMatch = false;
+        if (topics.length > 0) {
+          if (topics.includes(course.topic)) {
+            topicMatch = true;
+          }
+        }
+        else {
+          topicMatch = true;
+        } if (locations.length > 0) {
+          if (locations.includes(course.location)) {
+            locationMatch = true;
+          }
+        }
+        else {
+          locationMatch = true;
+        }
+        return topicMatch && locationMatch
+      })
+    }
+  }
+
+})
+
+var topicMatch = false, locationMatch = false;
+
+
+var filterApp = new Vue({
+el: '#filter',
+data: {
+  courses: courses,
+  selectedTopic:[],
+  selectedLocation: [],
+},methods: {
+  reset: function() {
+    this.selectedTopic = [];
+    this.selectedLocation = [];
+  }
+},
+computed: {
+  filteredCourses: function() {
+    var topics = this.selectedTopic,
+      locations = this.selectedLocation;
+    return this.courses.filter(function (course) {
+      var topicMatch = false,
+        locationMatch = false;
+      if (topics.length > 0) {
+        if (topics.includes(course.topic)) {
+          topicMatch = true;
+        }
+      }
+      else {
+        topicMatch = true;
+      }
+      if (locations.length > 0) {
+        if (locations.includes(course.location)) {
+          locationMatch = true;
+        }
+      }
+      else {
+        locationMatch = true;
+      }
+      return topicMatch && locationMatch
+    })
+  }
+}
+
+})
